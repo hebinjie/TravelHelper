044c20bc94cb8f9691a9b9036d18d009de642127
xingyi <973294773@qq.com>
2025-03-31 09:26:38 +0800

    跨域访问支持

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 # 从配置文件中导入日记文件路径配置
 from config import DIARY_FILE
 from config import USER_FILE
@@ -9,5 +10,7 @@ def create_app():
     # 将文件路径配置添加到 Flask 应用的配置中
     app.config['DIARY_FILE'] = DIARY_FILE
     app.config['USER_FILE'] = USER_FILE
+    # 配置跨域资源共享（CORS）
+    CORS(app, supports_credentials=True)  # 支持跨域请求，允许携带凭证（如 Cookies）
     # 返回创建并配置好的 Flask 应用实例
     return app
